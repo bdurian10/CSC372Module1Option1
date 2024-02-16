f9df79124c3b4ef0095c28eb0c8534f2f902ccab
Braden Durian <braden.durian@csuglobal.edu>
2024-02-15 20:52:32 -0700

    Second Commit

--- BankAccount.java
+++ BankAccount.java
@@ -1,5 +1,93 @@
 package module1option1;
 
 public class BankAccount {
+	
+	protected String firstName;
+	protected String lastName;
+	protected int accountID;
+	protected double balance;
+	
+	//Default Constructor
+	public BankAccount() {
+		
+		balance = 0;
+		accountID = 0;
+	
+	}
+	
+	//Parameterized constructor
+	public BankAccount(String firstName, String lastName, int accountID, double balance) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.accountID = accountID;
+		this.balance = balance;
+	}
+	
+	//Method to deposit money into an account
+	public double deposit(double deposit) {
+		
+		balance += deposit;
+		return balance;
+		
+	}
+	
+	public double withdrawal(double withdrawal) {
+		
+		balance -= withdrawal;
+		return balance;
+		
+	}
+	
+	public void setFirstName(String firstName) {
+		
+		this.firstName = firstName;
+		
+	}
+	
+	public String getFirstName() {
+		
+		return firstName;
+		
+	}
+	
+	public void setLastName(String lastName) {
+		
+		this.lastName = lastName;
+		
+	}
+	
+	public String getLastName() {
+		
+		return lastName;
+		
+	}
+	
+	public void setAccountID(int accountID) {
+		
+		this.accountID = accountID;
+		
+	}
+	
+	public int getAccountID() {
+		
+		return accountID;
+		
+	}
+	 
+	public double getBalance() {
+		
+		return balance;
+		
+	}
+	
+	//Prints account information
+	public void accountSummary() {
+		
+		System.out.println(lastName + ", " + firstName);
+		System.out.println("Account ID: " + accountID);
+		System.out.printf("Balance: %f.2\n", balance);
+		
+	}
 
 }
--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,5 +1,27 @@
 package module1option1;
 
-public class CheckingAccount {
+public class CheckingAccount extends BankAccount {
+	
+	protected double interestRate;
+	
+	//Processes withdrawal, if balance is negative, 
+	//withdrawals 30 additional dollars as an overdraft fee
+	//Returns string 
+	public String processWithdrawal() {
+		
+		if(this.getBalance() < 0) {
+			
+			this.withdrawal(30);
+			return "Overdraft Fee of $30 Applied to Your Account";
+		
+		}
+		
+		else {
+			
+			String withdrawalMessage = "Withdrawal Processed. New Balance: " + this.getBalance();
+			return withdrawalMessage;
+			
+		}
+	}
 
 }
